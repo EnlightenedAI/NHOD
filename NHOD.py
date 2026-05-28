c0257deecd3a1aa7dd0fa7fe747952ba4011f822
EnlightenedAI <67188977+EnlightenedAI@users.noreply.github.com>
2026-05-28 18:10:55 +0800

    Update NHOD.py

--- NHOD.py
+++ NHOD.py
@@ -3,93 +3,111 @@ import time
 import pyod
 from scipy.spatial import KDTree
 import numpy as np
-np.set_printoptions(threshold=np.inf)
-import scipy.io as scio    
+import scipy.io as scio
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import accuracy_score
-#a=np.array(range(1,21))*10
-K=np.array([61])  
-name=np.array([1])
-# K= np.arange(10,201,10)
-for na in range(name.shape[0]):
-    parts = ['../data/data','.mat']
-    namen=str(name[na])
-    print('Dataset:'+namen)
-    dataFile = parts[0]+namen+parts[1]
-    data = scio.loadmat(dataFile)
-    data1=data['data'][:,0:-1]
-    lable=data['data'][:,-1]
-    w=data1.shape[1]
-    print('number of objects：', data1.shape[0])
-    print('number of dimensions：',w)
-    T=np.sum(lable==1)          #number of outliers
-    print('number of outliers:',T)
-    min_max_scaler = MinMaxScaler()
-    Tdata = min_max_scaler.fit_transform(data1)
-    tree = KDTree(Tdata,leafsize=100000)
-    trandata = np.array([[row[i] for row in Tdata] for i in range(len(Tdata[0]))])
-    dist, ind = tree.query(Tdata, K[K.shape[0] - 1] + 1)
-    fpauc=open(r'./auc/auc.txt',"a+")
-    fpacc=open(r'./acc/ac.txt',"a+")
-    fptime=open(r'./time/time.txt',"a+")
-    #fpout=open(r'./out/out.txt',"a+")
-    print('data' + namen, 'K:', K, 'AUC',file=fpauc)
-    print('data' + namen, 'K:', K,'AC', file=fpacc)
-    print('data' + namen, 'K:', K, 'time', file=fptime)
-    #print('data' + namen, 'K:', K, 'out', file=fpout)
+from sklearn.metrics import roc_auc_score, accuracy_score
 
-    for k in K:
-        start = time.time()
-        print('k:', k)
-        N = 100
-        n = 0
-        print(math.ceil(Tdata.shape[0] / N))
-        for r in range(math.ceil(Tdata.shape[0] / N)):
-            if r < math.ceil(Tdata.shape[0] / N)-1:
-                minidata = Tdata[n:n + N, :]
-                miniind = ind[n:n + N, :]
-            else:
-                minidata = Tdata[n:Tdata.shape[0], :]
-                miniind = ind[n:Tdata.shape[0], :]
-            #print(minidata.shape)
-            n = n + N
-            minitrandata = np.array([[row[i] for row in minidata] for i in range(len(minidata[0]))])
-            # trandata= np.array([[row[i] for row in Tdata] for i in range(len(Tdata[0])) ])#type:[2,1043]
-            lcfk = np.zeros((w, len(minidata))) 
-            lcfkk = [] 
-            # data_1=data1/mdata
-            for kk in range(1, k + 1):
-                k1 = miniind[:, kk] 
-                k1data = trandata[:, k1]
-                moe = np.array(np.linalg.norm(minitrandata - k1data, axis=0, keepdims=True), dtype=np.float128)
-                lcf = (k1data - minitrandata) * (moe ** (w-2))
-                if kk==1:
-                    lcfk=lcf 
+# Configuration parameters
+k_values = np.array([61])
+dataset_indices = np.array([1])
+batch_size = 100
+
+for idx in dataset_indices:
+    dataset_name = str(idx)
+    print(f'Dataset: {dataset_name}')
+    
+    # Load dataset
+    data_file = f'../data/data{dataset_name}.mat'
+    mat_data = scio.loadmat(data_file)
+    features = mat_data['data'][:, 0:-1]
+    labels = mat_data['data'][:, -1]
+    
+    num_samples = features.shape[0]
+    num_features = features.shape[1]
+    num_outliers = np.sum(labels == 1)
+    
+    print(f'Number of objects: {num_samples}')
+    print(f'Number of dimensions: {num_features}')
+    print(f'Number of outliers: {num_outliers}')
+    
+    # Feature scaling
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(features)
+    
+    # Build KDTree for neighbor search
+    tree = KDTree(scaled_data, leafsize=100000)
+    transposed_data = scaled_data.T
+    
+    # Query nearest neighbors
+    max_k = k_values[-1]
+    distances, indices = tree.query(scaled_data, max_k + 1)
+    
+    # Open result files for appending
+    with open('./auc/auc.txt', "a+") as f_auc, \
+         open('./acc/ac.txt', "a+") as f_acc, \
+         open('./time/time.txt', "a+") as f_time:
+        
+        print(f'data{dataset_name}, K: {k_values}, AUC', file=f_auc)
+        print(f'data{dataset_name}, K: {k_values}, AC', file=f_acc)
+        print(f'data{dataset_name}, K: {k_values}, time', file=f_time)
+        
+        for k in k_values:
+            start_time = time.time()
+            print(f'k: {k}')
+            
+            # Batch process for LCF calculation
+            num_batches = math.ceil(num_samples / batch_size)
+            all_lcf = None
+            
+            for i in range(num_batches):
+                start_idx = i * batch_size
+                end_idx = min((i + 1) * batch_size, num_samples)
+                
+                mini_data = scaled_data[start_idx:end_idx, :]
+                mini_indices = indices[start_idx:end_idx, :]
+                mini_data_t = mini_data.T
+                
+                batch_lcf = np.zeros((num_features, mini_data.shape[0]))
+                
+                # Calculate LCF contribution for each neighbor
+                for neighbor_idx in range(1, k + 1):
+                    neighbor_indices = mini_indices[:, neighbor_idx]
+                    neighbor_data = transposed_data[:, neighbor_indices]
+                    
+                    # Calculate Local Correlation Factor components
+                    diff = neighbor_data - mini_data_t
+                    moe = np.array(np.linalg.norm(diff, axis=0, keepdims=True), dtype=np.float128)
+                    lcf_contribution = diff * (moe ** (num_features - 2))
+                    
+                    if neighbor_idx == 1:
+                        batch_lcf = lcf_contribution
+                    else:
+                        batch_lcf += lcf_contribution
+                
+                if i == 0:
+                    all_lcf = batch_lcf
                 else:
-                    lcfk = lcfk+lcf  
-            if r == 0:
-                LCF = lcfk
-            else:
-                LCF = np.hstack((LCF, lcfk))
-        lcod = np.linalg.norm(LCF, axis=0, keepdims=True)
-        end = time.time()
-        rLcf = lcod.T
-        for kk in range(0,k+1):
-            if kk==0:
-                NHOD=lcod[0,ind[:,kk]]
-            else:
-                NHOD=NHOD+lcod[0,ind[:,kk]]
-        NHOD=np.array(NHOD).reshape(len(data1),1)
-        AUC=roc_auc_score(lable,NHOD)
-        y_pred = pyod.utils.utility.get_label_n(lable,NHOD, T)
-        AC=accuracy_score(lable, y_pred)
-        print('Accuracy:',AC)
-        runtime=end-start
-        print('time:',runtime)
-        print(runtime, file=fptime)
-        print(AUC, file=fpauc)
-        print(AC, file=fpacc)
-    fpauc.close()
-    fpacc.close()
-    fptime.close()
+                    all_lcf = np.hstack((all_lcf, batch_lcf))
+            
+            # Calculate final LCOD scores
+            lcod_scores = np.linalg.norm(all_lcf, axis=0, keepdims=True)
+            end_time = time.time()
+            
+            # Aggregate scores across the neighborhood (NHOD)
+            nhod_scores = np.zeros((num_samples, 1))
+            for neighbor_idx in range(0, k + 1):
+                nhod_scores[:, 0] += lcod_scores[0, indices[:, neighbor_idx]]
+            
+            # Evaluate performance
+            auc_score = roc_auc_score(labels, nhod_scores)
+            pred_labels = pyod.utils.utility.get_label_n(labels, nhod_scores, num_outliers)
+            accuracy = accuracy_score(labels, pred_labels)
+            
+            runtime = end_time - start_time
+            print(f'Accuracy: {accuracy}')
+            print(f'Time: {runtime}')
+            
+            # Write results to files
+            print(runtime, file=f_time)
+            print(auc_score, file=f_auc)
+            print(accuracy, file=f_acc)
